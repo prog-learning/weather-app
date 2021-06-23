17e72809ba82c8f105ef936ff7780d9f3c93bee5
nbr41to <nbr.41to@gmail.com>
2021-06-23 17:22:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,85 @@
 import { useState, useEffect } from 'react';
 
-const API_KEY = '609c6c02a83e2f0521051bed0d6da340';
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&';
 
-// const cities = [
-//   { name: 'Cairns', id: 2172797 },
-//   { name: 'Los Angeles', id: 1705545 },
-//   { name: 'Linjiang', id: 1796228 },
-// ];
+const cities = ['aomori', 'tokyo', 'saitama', 'aiti', 'nagasaki'];
 
 function App() {
   const [weathers, setWeaters] = useState([]);
-  const [keyword, setKeyword] = useState('Cairns');
+  const [keywords, setKeywords] = useState([]);
 
   useEffect(() => {
     fetchAll();
   }, []);
 
   const fetchAll = async () => {
-    // Promise.all(cities.map(async(city)=>{
-    //   const data1 = await fetch(`${BASE_URL}?id=${city.id}&appid=${API_KEY}`);
-    //   const data11 = await data1.json();
-    // }))
+    const result = [];
+    await Promise.all(
+      cities.map(async (city) => {
+        const fetchData = await fetch(`${BASE_URL}&q=${city}&appid=${API_KEY}`);
+        const data = await fetchData.json();
+        result.push(data);
+      })
+    );
+    setWeaters(result);
+  };
 
-    const data1 = await fetch(`${BASE_URL}?id=2172797&appid=${API_KEY}`);
-    const data11 = await data1.json();
-
-    const data2 = await fetch(`${BASE_URL}?id=1705545&appid=${API_KEY}`);
-    const data21 = await data2.json();
-
-    const data3 = await fetch(`${BASE_URL}?id=1796228&appid=${API_KEY}`);
-    const data31 = await data3.json();
-
-    setWeaters([data11, data21, data31]);
+  const addKeyword = (value) => {
+    if (keywords.includes(value)) return;
+    setKeywords([...keywords, value]);
   };
 
   console.log(weathers);
   return (
     <div className='App'>
       <h1>Weather App!!</h1>
-      <select name='' id=''>
-        <option value=''>select</option>
-      </select>
       <div>
-        <h3>table view</h3>
-        {weathers
-          .filter((a) => a.name === keyword)
-          .map((a) => (
-            <li>
-              {a.name},{a.main.temp}
-            </li>
-          ))}
+        <h3>Filter keywords</h3>
+        {weathers.map((weather) => {
+          const { name, id } = weather;
+          return (
+            <button key={id} onClick={() => addKeyword(name)}>
+              {name}
+            </button>
+          );
+        })}
+        <button onClick={() => setKeywords([])}>reset</button>
       </div>
+      <br />
+      <div>
+        <span>Selected: </span>
+        {keywords.map((keyword) => (
+          <button id={keyword} disabled>
+            {keyword}
+          </button>
+        ))}
+      </div>
+      <h3>Today's weather</h3>
+      <table border='1'>
+        <tbody>
+          <tr align='center'>
+            <td>場所</td>
+            <td>天気</td>
+            <td>気温</td>
+            <td>湿度</td>
+          </tr>
+          {weathers
+            .filter((weather) => {
+              if (!keywords.length) return true;
+              return keywords.includes(weather.name);
+            })
+            .map((weather) => (
+              <tr key={weather.id}>
+                <td>{weather.name}</td>
+                <td>{weather.weather[0].main}</td>
+                <td>{weather.main.temp}</td>
+                <td>{weather.main.humidity}%</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <button onClick={fetchAll}>reload</button>
     </div>
   );
 }
